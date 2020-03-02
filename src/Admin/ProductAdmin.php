5a53a68ab8c93393cfa952c87fdb9a0991a85ab4
highmoonrays <steachy.hm.123@gmail.com>
2020-03-02 11:00:21 +0300

    working with custom validation in sonata admin forms

--- src/Admin/ProductAdmin.php
+++ src/Admin/ProductAdmin.php
@@ -4,17 +4,22 @@ declare(strict_types=1);
 
 namespace App\Admin;
 
-use App\Form\DataTransferObject\ProductDTO;
-use App\Form\ProductType;
+use App\Service\ImportTool\FileDataValidator;
+use App\Validator\CustomUniqueEntity;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Sonata\Form\Validator\ErrorElement;
+use Symfony\Component\Validator\Constraints\LessThan;
 
+/**
+ * Class ProductAdmin
+ * @package App\Admin
+ */
 final class ProductAdmin extends AbstractAdmin
 {
-
     /**
      * @param DatagridMapper $datagridMapper
      */
@@ -87,5 +92,52 @@ final class ProductAdmin extends AbstractAdmin
             ->add('stock')
             ->add('cost')
             ;
+    }
+
+    /**
+     * @param ErrorElement $errorElement
+     * @param object $object
+     * @throws \Exception
+     */
+    public function validate(ErrorElement $errorElement, $object)
+    {
+        $errorElement
+            ->with('name')
+                ->assertNotBlank()
+            ->end()
+            ->with('description')
+                ->assertNotBlank()
+            ->end()
+            ->with('code')
+                ->assertNotBlank()
+            ->end()
+            ->with('cost')
+                ->addConstraint(new LessThan(1000))
+                ->assertNotBlank()
+            ->end()
+            ->with('stock')
+                ->assertNotBlank()
+            ->end()
+        ;
+
+        if ($errorElement->getSubject()->getCost() < 5 && $errorElement->getSubject()->getStock() < 10) {
+            $errorElement->with('stock')
+                ->addViolation(
+                    'Stock is less than '.FileDataValidator::PRODUCT_RULE_STOCK_MIN_RULE
+                    .' and cost is less than '. FileDataValidator::PRODUCT_RULE_MIN_COST)
+                ->end();
+        }
+
+        $container = $this->getConfigurationPool()->getContainer();
+        $em = $container->get('doctrine.orm.entity_manager');
+        $foundedObject = $em->getRepository(get_class($errorElement->getSubject()))->findOneByCode($errorElement->getSubject()->getCode());
+
+        if ($foundedObject) {
+            if ($errorElement->getSubject()->getId() != $foundedObject->getId()) {
+                $errorElement->with('code')
+                    ->addViolation('Product with this code is already existing!')
+                    ->end();
+            }
+        }
     }
 }
--- src/Controller/ProductAdminController.php
+++ src/Controller/ProductAdminController.php
@@ -8,15 +8,4 @@ use Sonata\AdminBundle\Controller\CRUDController;
 
 final class ProductAdminController extends CRUDController
 {
-    public function editAction($id = null)
-    {
-        return $this->redirectToRoute('editProduct', [
-            'id' => $id,
-        ]);
-    }
-
-    public function  createAction()
-    {
-        return $this->redirectToRoute('createNewProduct');
-    }
 }
